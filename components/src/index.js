bfa44e0d5b87a0e05010ad26f3c799de5e87ded4
ratko <ratko.gjurichanin@gmail.com>
2019-12-03 12:06:43 -0600

    Wrapping up One component with another

--- components/src/index.js
+++ components/src/index.js
@@ -7,25 +7,31 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        img={faker.image.avatar()}
-        author="Sam"
-        timeAgo="Today At 4:30PM"
-        comment="Awesome!!!"
-      />
-      <CommentDetail
-        img={faker.image.avatar()}
-        author="Alex"
-        timeAgo="Dec 1 at 12AM"
-        comment="Great Desk Setup!!!"
-      />
-      <CommentDetail
-        img={faker.image.avatar()}
-        author="Zoran"
-        timeAgo="Yesterday At 9AM"
-        comment="Keep Going Ratko..."
-      />
-      <ApprovalCard />
+      <ApprovalCard>
+        <CommentDetail
+          img={faker.image.avatar()}
+          author="Sam"
+          timeAgo="Today At 4:30PM"
+          comment="Awesome!!!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          img={faker.image.avatar()}
+          author="Alex"
+          timeAgo="Dec 1 at 12AM"
+          comment="Great Desk Setup!!!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          img={faker.image.avatar()}
+          author="Zoran"
+          timeAgo="Yesterday At 9AM"
+          comment="Keep Going Ratko..."
+        />
+      </ApprovalCard>
     </div>
   );
 };
