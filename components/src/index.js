21a5020a034003498c380926b99cf5dc2cba5a6e
ratko <ratko.gjurichanin@gmail.com>
2019-12-03 09:32:38 -0600

    Copy/Paste 2 times the comments
    Naive Component Approach LOOKS MESSY

--- components/src/index.js
+++ components/src/index.js
@@ -19,6 +19,34 @@ const App = () => {
           <div className="text">Nice Setup Ratko</div>
         </div>
       </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="Avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Natali
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 7AM</span>
+          </div>
+          <div className="text">Nice Setup Ratko</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="Avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Natali
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 7AM</span>
+          </div>
+          <div className="text">Nice Setup Ratko</div>
+        </div>
+      </div>
     </div>
   );
 };
