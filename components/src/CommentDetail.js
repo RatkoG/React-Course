59e28e1081857b7a17c50f04d08f54bab760032e
ratko <ratko.gjurichanin@gmail.com>
2019-12-03 09:44:11 -0600

    Extracting JSX to New Component

---
+++ components/src/CommentDetail.js
@@ -0,0 +1,19 @@
+import React from "react";
+const CommentDetail = () => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="Avatar" src={faker.image.avatar()} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Natali
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 7AM</span>
+        </div>
+        <div className="text">Nice Setup Ratko</div>
+      </div>
+    </div>
+  );
+};
