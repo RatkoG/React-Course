7c6a53e52e9b732c7f433f0845c2c4de5318c8f1
ratko <ratko.gjurichanin@gmail.com>
2019-12-03 09:10:22 -0600

    Building first comment block

--- components/src/index.js
+++ components/src/index.js
@@ -2,7 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hi There</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="Avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Natali
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 7AM</span>
+          </div>
+          <div className="text">Nice Setup Ratko</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
