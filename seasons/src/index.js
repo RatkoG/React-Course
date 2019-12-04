5c9d24bba659a7d1155e67e29c6dff81fb9b96d5
ratko <ratko.gjurichanin@gmail.com>
2019-12-04 11:00:15 -0600

    Updating State Properties

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,18 +4,23 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-
+    // THIS IS THE ONLY TIME we do direct assignment to this.state
     this.state = {
       lat: null
     };
+    // Ova go premestivme. Za da ne se povikuva na sekoe od render ko che imame update na componento. Ednas e povikano i posle mozi render da se apdejtira kolku so saka pati...
+    window.navigator.geolocation.getCurrentPosition(
+      // console.log(position)
+      position => {
+        // We called setState
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => console.log(err)
+    );
   }
   // React says we have to define render!!!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
-    return <div>Latitude:</div>;
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
