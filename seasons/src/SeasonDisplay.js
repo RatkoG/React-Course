2fdfdbdf358fd1654f71e6c376d4b5da69ab1cb2
ratko <ratko.gjurichanin@gmail.com>
2019-12-04 14:43:20 -0600

    Refactoring duplicate ternary operators
    Ova e nekoj mnogu dobar patern so koristenje na object

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,5 +1,14 @@
 import React from "react";
-
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr, it is chilly",
+    iconName: "snowflake"
+  }
+};
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -11,16 +20,14 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {
   // console.log(props.lat);
   const season = getSeason(props.lat, new Date().getMonth());
-  const text =
-    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
-  const icon = season === "winter" ? "snowflake" : "sum";
+  const { text, iconName } = seasonConfig[season]; //{text,iconName}
   // console.log(season);
 
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
