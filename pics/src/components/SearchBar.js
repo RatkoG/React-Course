8e874066dbefa27bea99df2f3c8d368ba2096e52
ratko <ratko.gjurichanin@gmail.com>
2019-12-05 10:50:42 -0600

    Prevent default on submit refresh
    Ova che ni daj error zaradi this
    Ima lekcija zosto e to taka

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,11 +2,15 @@ import React from "react";
 
 class SearchBar extends React.Component {
   state = { term: "" };
+  onFormSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.term);
+  }
 
   render() {
     return (
       <div className="ui segment">
-        <form action="/" className="ui form">
+        <form onSubmit={this.onFormSubmit} action="/" className="ui form">
           <div className="field">
             <label htmlFor="/">Image Search</label>
             <input
