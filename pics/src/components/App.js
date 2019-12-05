00d5110126090a00044e8128701881ee52e2a56d
ratko <ratko.gjurichanin@gmail.com>
2019-12-05 17:57:13 -0600

    this is so Async Await

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -2,18 +2,15 @@ import React from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar";
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID 81ef353e9c204392060ad2dbfadc962a37c8451e8b4a5baf9eee999f8b3c6401"
-        }
-      })
-      .then(response => {
-        console.log(response.data.results);
-      });
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 81ef353e9c204392060ad2dbfadc962a37c8451e8b4a5baf9eee999f8b3c6401"
+      }
+    });
+    console.log(response.data.results);
   }
   render() {
     return (
