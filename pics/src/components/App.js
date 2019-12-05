b582b1486ca7e86b4b5a9c2cc83c04a71db0bb89
ratko <ratko.gjurichanin@gmail.com>
2019-12-05 17:53:35 -0600

    Ova e eden nacin da se praj Request odnostno pring

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,13 +3,17 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 class App extends React.Component {
   onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization:
-          "Client-ID 81ef353e9c204392060ad2dbfadc962a37c8451e8b4a5baf9eee999f8b3c6401"
-      }
-    });
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID 81ef353e9c204392060ad2dbfadc962a37c8451e8b4a5baf9eee999f8b3c6401"
+        }
+      })
+      .then(response => {
+        console.log(response.data.results);
+      });
   }
   render() {
     return (
