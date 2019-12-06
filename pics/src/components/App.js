340eac6cc7c4e5e296e34b1c971c8a991775338e
ratko <ratko.gjurichanin@gmail.com>
2019-12-05 18:02:18 -0600

    initializing state
    Zavistnost so ocekuvame vnatre da ima kako property togas to stavame prazno. vo ovaj slucaj e sega empty array imase prethodno null i empty sting

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar";
 class App extends React.Component {
+  state = { images: [] };
   async onSearchSubmit(term) {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
