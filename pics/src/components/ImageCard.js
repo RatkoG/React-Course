62fb2d43526e09e7c4db368e69075e27dbf5d4a7
ratko <ratko.gjurichanin@gmail.com>
2019-12-06 18:29:38 -0600

    Accesing image Height

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+  componentDidMount() {
+    //Ova ovde nema da kazuva odma clientHeught oti vo momento slikata nema da bidi pojavena
+    // console.log(this.imageRef);
+    // console.log(this.imageRef.current.clientHeight);
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+  // Ova e zaradi this problemo so go imame
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
     const { description, urls } = this.props.image;
     return (
       <div>
-        <img src={urls.regular} alt={description} />
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
