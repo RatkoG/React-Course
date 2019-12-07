3b92e60a3fb41a44ccb3f52fcdc831acf0114f59
ratko <ratko.gjurichanin@gmail.com>
2019-12-07 14:33:28 -0600

    Formata da raboti on change. Input text raboti

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { myInput: "" };
+  onInputChange = e => this.setState({ myInput: e.target.value });
   render() {
+    console.log(this.state.myInput);
     return (
       <div className="ui segment search-bar">
         <form action="#" className="ui form">
           <div className="field">
             <label htmlFor="#">Video Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.myInput}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
