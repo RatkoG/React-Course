b7e440e9143f2171f8abfcb02efa06beb35e1325
ratko <ratko.gjurichanin@gmail.com>
2019-12-10 13:35:02 -0600

    Updeting the state with selected song
    I Objasnuvase zosto na povukavme selectedSong funkcijata od connect a ne od componend.

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import { selectedSong } from "../actions";
 class SongList extends Component {
   renderList() {
     return this.props.songs.map(song => {
       return (
         <div className="item" key="song.title">
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -18,12 +23,16 @@ class SongList extends Component {
   render() {
     // this.props === { songs: state.songs };
     // console.log(this.props);
+    console.log(this.props);
 
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = state => {
+  // Ova e za select ko che se stegni
+  console.log(state);
+
   // Ova e da se isprinta vo console
   // console.log(state);
   // return state;
@@ -31,4 +40,6 @@ const mapStateToProps = state => {
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+  selectSong: selectedSong
+})(SongList);
