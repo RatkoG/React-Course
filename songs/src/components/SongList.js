201e40952ce53d0914178afe0ec2e80b388acf47
ratko <ratko.gjurichanin@gmail.com>
2019-12-10 12:45:26 -0600

    CONSOL LOG OUT OVA E VO SEKOJ PROEKT SO CE GO RABOTIS

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -3,13 +3,19 @@ import { connect } from "react-redux";
 
 class SongList extends Component {
   render() {
+    // this.props === { songs: state.songs };
+    console.log(this.props);
+
     return <div>SongList</div>;
   }
 }
 
 const mapStateToProps = state => {
-  console.log(state);
-  return state;
+  // Ova e da se isprinta vo console
+  // console.log(state);
+  // return state;
+
+  return { songs: state.songs };
 };
 
 export default connect(mapStateToProps)(SongList);
