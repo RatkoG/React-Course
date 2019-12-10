5dc4d5a96e679477ef8377f78e48dcd5774aecd5
ratko <ratko.gjurichanin@gmail.com>
2019-12-10 12:31:34 -0600

    The Connect function
    Connect e vo SongList ne e na posebno mesto oti samo na SongList sega mu e potrebno da go kontaktira Provicer za da gi zemi listata na songs

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import SongList from "./SongList";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
