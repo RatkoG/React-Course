93903b1ea8bf54c5152ec8aec3e7b6c686282d47
ratko <ratko.gjurichanin@gmail.com>
2019-12-12 13:30:53 -0600

    refactoring

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,8 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-  return async function(dispatch, getState) {
-    const response = await jsonPlaceholder.get("/posts");
-    dispatch({ type: "FETCH_POSTS", payload: response });
-  };
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get("/posts");
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
 
 // TALLY FINE da koristime ova
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -5,10 +5,10 @@ import { fetchPosts } from "../actions";
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
-    console.log(this.props.fetchPosts);
+    // console.log(this.props.fetchPosts);
   }
   render() {
-    console.log("I was triggered during render");
+    // console.log("I was triggered during render");
     return <div>Post List</div>;
   }
 }
