266dc926355f378e4484ecf62dc5aabd592ebc97
ratko <ratko.gjurichanin@gmail.com>
2019-12-12 10:32:57 -0600

    Setup project
    We add dummy reducer to bypass the console error. After that we will need to replace it with valid one.

---
+++ blog/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+  // This is to get the erroe message away cause we dont know what we will need
+  dummy: () => "Hi there"
+});
