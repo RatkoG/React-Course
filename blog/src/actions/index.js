2871100838aef1d30cd27403e3ee063e0436ecde
ratko <ratko.gjurichanin@gmail.com>
2019-12-12 12:29:57 -0600

    Preconfig Axios ERROR che se javi | Error: Actions must be plain objects. Use custom middleware for async actions.
    Ovde probuvame to call API so Asynx Await i zarati ova syntax imame problem imame error.... oti ko che bidi kodo compiled ova nema da vrati plain object. Vo Notion imas video i objasnato ubo

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,9 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+export const fetchPosts = async () => {
+  // BAD APPROACH !!!!
+  const response = await jsonPlaceholder.get("/posts");
   return {
-    type: "FETCH_POSTS"
+    type: "FETCH_POSTS",
+    payload: response
   };
 };
