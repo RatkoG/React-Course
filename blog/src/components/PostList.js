326242b7fb8630590cd486190a9cb1f9415add06
ratko <ratko.gjurichanin@gmail.com>
2019-12-12 11:56:15 -0600

    Wiring up Action Creator

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+    console.log(this.props.fetchPosts);
+  }
   render() {
+    console.log("I was triggered during render");
     return <div>Post List</div>;
   }
 }
-
-export default PostList;
+// NULL e oti sega zasega nemame mapStateToProps
+export default connect(null, { fetchPosts })(PostList);
