3d3ad6a3de4c90feed3fd0d1a84e4877ab5cc16d
ratko <ratko.gjurichanin@gmail.com>
2019-12-12 13:28:34 -0600

    Wiring up REDUX thunk as a middleware THIS IS HOW WE HOOK UP
    applyMiddleware e kako povrsuvame redux-thunk so redux store

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,16 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
 export const fetchPosts = () => {
-  // BAD APPROACH !!!!
-  const promise = jsonPlaceholder.get("/posts");
-  return {
-    type: "FETCH_POSTS",
-    payload: promise
+  return async function(dispatch, getState) {
+    const response = await jsonPlaceholder.get("/posts");
+    dispatch({ type: "FETCH_POSTS", payload: response });
   };
 };
+
+// TALLY FINE da koristime ova
+// export const selectPost =()=>{
+//   return{
+//     type:'SELECT_POST',
+//     payload:promise
+//   }
+// }
