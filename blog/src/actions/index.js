7c776c962e265cd1cc73e39a5e6bf5a50e353111
ratko <ratko.gjurichanin@gmail.com>
2019-12-14 14:47:58 -0600

    Dispatching Correct Values
    Ne sakame se so che se vrati od API sakame samo datata vo array so e

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -2,7 +2,8 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
-  dispatch({ type: "FETCH_POSTS", payload: response });
+  // Ovde e response.data oti sakame samo data da vratime a ne se i sesto
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
 // TALLY FINE da koristime ova
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -8,9 +8,16 @@ class PostList extends React.Component {
     // console.log(this.props.fetchPosts);
   }
   render() {
+    // Ovde che imame
+    console.log(this.props.posts);
+
     // console.log("I was triggered during render");
     return <div>Post List</div>;
   }
 }
+
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
 // NULL e oti sega zasega nemame mapStateToProps
-export default connect(null, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPosts })(PostList);
