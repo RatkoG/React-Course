e1a3e398862ccf77965426e3e2fb625476cff294
ratko <ratko.gjurichanin@gmail.com>
2019-12-16 14:23:57 -0600

    Applying memoize
    Ova e tricky part part   I ova e kako so treba da raboti

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
@@ -5,11 +6,23 @@ export const fetchPosts = () => async dispatch => {
   // Ovde e response.data oti sakame samo data da vratime a ne se i sesto
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
-
-export const fetchUser = id => async dispatch => {
+// Ova e stvarno sotreba da go prajme
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch);
+};
+// Private function should not be called unless you know what are you doing
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
+
+// Ova ovde pak ne raboti kako so treba
+// export const fetchUser = function(id) {
+//   return _.memoize(async function(dispatch) {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: "FETCH_USER", payload: response.data });
+//   });
+// };
 
 // TALLY FINE da koristime ova
 // export const selectPost =()=>{
