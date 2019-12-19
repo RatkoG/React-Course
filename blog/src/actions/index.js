7eea9214d5d2bca196675781cf19c7928890c633
ratko <ratko.gjurichanin@gmail.com>
2019-12-19 07:30:37 -0600

    Ovde Zastanav SE VRATIV NA POCETOKO OD APLIKACIJATA

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,12 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   console.log("About to fetch posts!");
   await dispatch(fetchPosts());
+  // getState().posts;
   console.log("fetched posts!");
+  console.log(getState().posts);
 };
 
 export const fetchPosts = () => async dispatch => {
