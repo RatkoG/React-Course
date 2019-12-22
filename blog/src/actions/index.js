9c1c06a1593a7fa1a5df6e7fff224473352f16a7
ratko <ratko.gjurichanin@gmail.com>
2019-12-22 15:35:11 -0600

    Finding Unique User Ids
    Ova e vtorio nacin. Ovde ce se izbrisa duplikatite  i e napraeno za reusability...

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,15 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPostsAndUsers = () => async dispatch => {
-  console.log("About to fetch posts!");
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  // console.log("About to fetch posts!");
   await dispatch(fetchPosts());
-  console.log("fetched posts!");
+  // getState().posts;
+  // console.log("fetched posts!");
+  // console.log(getState().posts);
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+  // console.log(userIds);
 };
 
 export const fetchPosts = () => async dispatch => {
