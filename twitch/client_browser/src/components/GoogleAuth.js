0ef5cd99ac0898f939d0ff2343a23d0568ea8301
ratko <ratko.gjurichanin@gmail.com>
2019-12-27 16:42:02 -0600

    Handling Auth Status Through Redux
    Ovde pocnuva da raboti aplikacijata pak SignIn Sign Out so Redux

--- twitch/client_browser/src/components/GoogleAuth.js
+++ twitch/client_browser/src/components/GoogleAuth.js
@@ -2,7 +2,6 @@ import React from "react";
 import { connect } from "react-redux";
 import { signIn, signOut } from "../action";
 class GoogleAuth extends React.Component {
-  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
       window.gapi.client
@@ -13,7 +12,7 @@ class GoogleAuth extends React.Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
@@ -33,9 +32,9 @@ class GoogleAuth extends React.Component {
     this.auth.signOut();
   };
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return null;
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
@@ -56,4 +55,11 @@ class GoogleAuth extends React.Component {
     return <div>{this.renderAuthButton()}</div>;
   }
 }
-export default connect(null, { signIn, signOut })(GoogleAuth);
+
+const mapStateToProps = state => {
+  return {
+    isSignedIn: state.auth.isSignedIn
+  };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
