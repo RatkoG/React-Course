d83ad201494f977ce4944dc48953d6818e9ad114
ratko <ratko.gjurichanin@gmail.com>
2019-12-27 19:46:19 -0600

    Automaticly handling Events

--- twitch/client_browser/src/components/streams/StreamCreate.js
+++ twitch/client_browser/src/components/streams/StreamCreate.js
@@ -1,7 +1,30 @@
 import React from "react";
+//Field = Component
+//reduxForm = Function
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    console.log(formProps);
 
-export default StreamCreate;
+    return (
+      <input
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      />
+    );
+  }
+  render() {
+    // console.log(this.props);
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "StreamCreate"
+})(StreamCreate);
