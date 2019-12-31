020560393062d910c71a21377db6c757235cea28
ratko <ratko.gjurichanin@gmail.com>
2019-12-30 18:54:01 -0600

    Fetching a Stream for Edit Stream

--- twitch/client_browser/src/components/streams/StreamEdit.js
+++ twitch/client_browser/src/components/streams/StreamEdit.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
-
-const StreamEdit = props => {
-  console.log(props);
-
-  return <div>StreamEdit</div>;
-};
+import { fetchStream } from "../../action";
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    console.log(this.props);
+    //This if statement is cause Title will be rendered a bit later
+    if (!this.props.stream) {
+      return <div>Loading</div>;
+    }
+    return <div>{this.props.stream.title}</div>;
+  }
+}
 const mapStateToProps = (state, ownProps) => {
   // console.log(ownProps);
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
