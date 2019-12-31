e9e8a1c51ff132192de4fd9e9ada50e4139fc2e2
ratko <ratko.gjurichanin@gmail.com>
2019-12-30 19:58:12 -0600

    StreamCreate

---
+++ twitch/client_browser/src/components/StreamForm.js
@@ -0,0 +1,72 @@
+import React from "react";
+//Field = Component
+//reduxForm = Function
+import { Field, reduxForm } from "redux-form";
+
+class StreamForm extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+    // console.log(formProps);
+    // console.log(meta);
+
+    return (
+      <div className={className}>
+        <label htmlFor="">{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit = formValues => {
+    //Ne ni treba ova vo redux
+    // event.preventDefault();
+    // console.log(formValues);
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    // console.log(this.props);
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+  //Ovie so se vo error primer title ili description se povrzuva so name vo Field gore...
+  if (!formValues.title) {
+    //only ran if the user did not enter a title
+    errors.title = "Please enter a title";
+  }
+  if (!formValues.description) {
+    errors.description = "Please enter a description";
+  }
+  return errors;
+};
+
+export default reduxForm({
+  form: "StreamForm",
+  validate
+})(StreamForm);
