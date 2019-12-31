e0b357c627d731c8ad9a9908b1c291fec2b482fa
ratko <ratko.gjurichanin@gmail.com>
2019-12-31 11:08:27 -0600

    Setting Initial Values
    This is when you click edit is taking the stream and wrigint out the values in input form

--- twitch/client_browser/src/components/streams/StreamEdit.js
+++ twitch/client_browser/src/components/streams/StreamEdit.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../action";
+import { fetchStream, editStream } from "../../action";
+import StreamForm from "./StreamForm";
+import { formValues } from "redux-form";
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+  onSubmit = formValues => {
+    console.log(formValues);
+  };
+
   render() {
     console.log(this.props);
     //This if statement is cause Title will be rendered a bit later
     if (!this.props.stream) {
       return <div>Loading</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={this.props.stream}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
 const mapStateToProps = (state, ownProps) => {
@@ -19,4 +33,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
