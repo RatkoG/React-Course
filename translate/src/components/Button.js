b1583d37017191e400a784a637645de3d9396b68
ratko <ratko.gjurichanin@gmail.com>
2020-01-04 13:13:45 -0600

    Accessing Data with Consumers instead of this.context
    static contextType we need only if we use this.context

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,12 +1,15 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
 class Button extends React.Component {
-  static contextType = LanguageContext;
-
   render() {
     // console.log(this.context);
-    const text = this.context === "english" ? "Submit" : "Испрати";
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => (value === "english" ? "Submit" : "Испрати")}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
