2f5e08abadc45e920a333dc40443e1bf93e525f5
ratko <ratko.gjurichanin@gmail.com>
2020-01-04 12:49:35 -0600

    Consuming the Context Value

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -4,8 +4,9 @@ class Button extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    console.log(this.context);
-    return <button className="ui button primary">Submit</button>;
+    // console.log(this.context);
+    const text = this.context === "english" ? "Submit" : "Podnesi";
+    return <button className="ui button primary">{text}</button>;
   }
 }
 
--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import LanguageContext from "../contexts/LanguageContext";
 class Field extends React.Component {
+  static contextType = LanguageContext;
   render() {
+    const text = this.context === "english" ? "Name" : "Ime";
     return (
       <div className="ui field">
-        <label htmlFor="">Name</label>
+        <label htmlFor="">{text}</label>
         <input type="text" name="" id="" />
       </div>
     );
