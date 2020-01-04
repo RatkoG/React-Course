6af7feeac5a007e6ec046a217f374ba6c370eafe
ratko <ratko.gjurichanin@gmail.com>
2020-01-04 11:02:46 -0600

    Player Building Better way
    We wont show video stream until we are sure that we got the stream

--- twitch/client_browser/src/components/streams/StreamShow.js
+++ twitch/client_browser/src/components/streams/StreamShow.js
@@ -11,7 +11,22 @@ class StreamShow extends React.Component {
 
   componentDidMount() {
     const { id } = this.props.match.params;
+
+    // console.log(this.videoRef);
+
     this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
     this.player = flv.createPlayer({
       type: "flv",
       url: `http://localhost:8000/live/${id}.flv`
@@ -19,6 +34,7 @@ class StreamShow extends React.Component {
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
   }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
